c9233fb93b8ebe6aa8d3066822ac4b83294a8f14
dmitriy.sosedov <d.i.sosedov@evrone.team>
2024-08-13 11:19:58 +0300

    [ERP-1621] Preprocess unlink for task commits

--- main.py
+++ main.py
@@ -4,7 +4,6 @@ import re
 from environs import Env
 from github import Github  # type: ignore  # pylint: disable=no-name-in-module
 
-from config.logger_config import logger
 from helpers.github import change_pull_request_body, formatted_line
 from helpers.yandex_tracker import YandexTracker
 
@@ -18,27 +17,56 @@ TASK_KEY_PATTERN = re.compile(r"[^[]*\[([^]]*)\]")
 
 
 def process_commits(commits, pr_number):
-    all_pulls = []
-    tasks = {}
+    tasks = []
+    added_pulls = []
     for commit in commits:
         pulls = commit.get_pulls()
+        is_pr_commit = False
+        sub_commits = []
         for pull in pulls:
-            if pull.number != pr_number:
-                all_pulls.append(pull)
+            if pull.number != pr_number and pull.number not in added_pulls:
                 all_matches = TASK_KEY_PATTERN.findall(pull.title)
                 for task_key in set(all_matches):
-                    tasks[task_key] = pull
-    return all_pulls, tasks
+                    tasks.append(
+                        {
+                            "number": pull.number,
+                            "message": None,
+                            "author": pull.user.login,
+                            "task_key": task_key,
+                        }
+                    )
+                added_pulls.append(pull.number)
+                is_pr_commit = True
+            else:
+                if "Merge pull request" in commit.commit.message:
+                    continue
+                sub_commits.append(
+                    {
+                        "number": pull.number,
+                        "message": commit.commit.message,
+                        "author": commit.author.login,
+                        "task_key": None,
+                    }
+                )
+        if not is_pr_commit and sub_commits:
+            tasks += sub_commits
+    return tasks
 
 
 def build_task_lines(tasks, yandex_tracker):
     result = []
-    for task_key, pr in tasks.items():
-        title = yandex_tracker.get_issue_summary(task_key)
-        if not title:
-            logger.info("[%s] (No task found)", task_key)
-            continue
-        task_line = formatted_line(task_key, title, pr.user.login, pr.number)
+    for task in tasks:
+        task_key = task.get("task_key")
+        author = task.get("author")
+        number = task.get("number")
+        if task_key:
+            title = yandex_tracker.get_issue_summary(task_key)
+            if not title:
+                print(f"[{task_key}] (No task found)")
+                continue
+            task_line = formatted_line(task_key, title, author, number)
+        else:
+            task_line = formatted_line(task_key, task.get("message"), author, number)
         result.append(task_line)
     return result
 
@@ -54,13 +82,9 @@ def main():
     commits = pull_request.get_commits()
 
     # Collect commits and task numbers
-    all_pulls, tasks = process_commits(commits, pr_number)
+    tasks = process_commits(commits, pr_number)
     result = build_task_lines(tasks, yandex_tracker)
     change_pull_request_body(pull_request, result)
-
-    for pull in all_pulls:
-        logger.info(pull.merge_commit_sha)
-        logger.info(pull.title)
 
 
 if __name__ == "__main__":
